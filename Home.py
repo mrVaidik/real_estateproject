86b060e840a80cfbe49e010a67a325947b991d51
mrVaidik <144434282+mrVaidik@users.noreply.github.com>
2024-05-24 20:49:21 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Home',
+    page_icon=':',
+)
+st.title("main page")
+st.sidebar.success("Select a page above")
+
+st.title('Welcome')
+
+
+
