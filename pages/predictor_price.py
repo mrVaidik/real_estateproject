6520988612147f131f9adf6f3b88328416523ffc
mrVaidik <144434282+mrVaidik@users.noreply.github.com>
2024-05-25 13:49:33 +0530

    Update predictor_price.py

--- pages/predictor_price.py
+++ pages/predictor_price.py
@@ -26,46 +26,45 @@ except ModuleNotFoundError as e:
 
 
 
+with st.form("my_form"):
+    st.header('Enter your inputs ')
+
+    property_type = st.selectbox('Property Type', ['flat', 'house'])
+
+    sector = st.selectbox('sector' , sorted(df ['sector'].unique().tolist()) )
+
+    bedroom  = float(st.selectbox('bedRoom', sorted(df['bedRoom'].unique().tolist()) ))
+
+    bathroom  = float(st.selectbox('bathroom', sorted(df['bathroom'].unique().tolist()) ))
+
+    balcony  = st.selectbox('balcony', sorted(df['balcony'].unique().tolist()) )
+
+    property_age = st.selectbox('property_age', sorted(df['agePossession'].unique().tolist()))
+
+    built_up_area = float(st.number_input('Built_up_area'))
+
+    servent_room = float(st.selectbox('servent_room', ['0.0', '1.0']))
+
+    store_room = float(st.selectbox('store_room', ['0.0', '1.0']))
+
+    furnishing = st.selectbox('furnishing', sorted(df['furnishing_type'].unique().tolist()))
+
+    luxury_category = st.selectbox('luxury_category', sorted(df['luxury_category'].unique().tolist()))
+
+    floor_category = st.selectbox('floor_category', sorted(df['floor_category'].unique().tolist()))
 
 
-st.header('Enter your inputs ')
+    if st.button('Predict'):
 
-property_type = st.selectbox('Property Type', ['flat', 'house'])
-
-sector = st.selectbox('sector' , sorted(df ['sector'].unique().tolist()) )
-
-bedroom  = float(st.selectbox('bedRoom', sorted(df['bedRoom'].unique().tolist()) ))
-
-bathroom  = float(st.selectbox('bathroom', sorted(df['bathroom'].unique().tolist()) ))
-
-balcony  = st.selectbox('balcony', sorted(df['balcony'].unique().tolist()) )
-
-property_age = st.selectbox('property_age', sorted(df['agePossession'].unique().tolist()))
-
-built_up_area = float(st.number_input('Built_up_area'))
-
-servent_room = float(st.selectbox('servent_room', ['0.0', '1.0']))
-
-store_room = float(st.selectbox('store_room', ['0.0', '1.0']))
-
-furnishing = st.selectbox('furnishing', sorted(df['furnishing_type'].unique().tolist()))
-
-luxury_category = st.selectbox('luxury_category', sorted(df['luxury_category'].unique().tolist()))
-
-floor_category = st.selectbox('floor_category', sorted(df['floor_category'].unique().tolist()))
-
-
-if st.button('Predict'):
-
-    data = [[property_type, sector, bedroom, bathroom, balcony,property_age, built_up_area, servent_room , store_room , furnishing , luxury_category, floor_category]]
-    columns = ['property_type', 'sector', 'bedRoom', 'bathroom', 'balcony',
+        data = [[property_type, sector, bedroom, bathroom, balcony,property_age, built_up_area, servent_room , store_room , furnishing , luxury_category, floor_category]]
+        columns = ['property_type', 'sector', 'bedRoom', 'bathroom', 'balcony',
                'agePossession', 'built_up_area', 'servant room', 'store room',
                'furnishing_type', 'luxury_category', 'floor_category']
 
-    one_df = pd.DataFrame(data , columns=columns)
-    st.dataframe(one_df)
+        one_df = pd.DataFrame(data , columns=columns)
+        st.dataframe(one_df)
 
-    base_price =  np.expm1(pipeline.predict(one_df))[0]
-    low = base_price - 0.21
-    high = base_price + 0.21
-    st.text("The Price of the {} is between {} CR and {} CR".format(property_type, round(low,2), round(high,2)))
+        base_price =  np.expm1(pipeline.predict(one_df))[0]
+        low = base_price - 0.21
+        high = base_price + 0.21
+        st.text("The Price of the {} is between {} CR and {} CR".format(property_type, round(low,2), round(high,2)))
