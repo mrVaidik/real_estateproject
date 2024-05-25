2ab3f97c31a8e3c3f233f92dd02e454e114ade48
mrVaidik <144434282+mrVaidik@users.noreply.github.com>
2024-05-25 11:15:04 +0530

    Update predictor_price.py

--- pages/predictor_price.py
+++ pages/predictor_price.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pickle
 import bz2
 import numpy as np
+import sklearn
 st.set_page_config(page_title="Predictive Price App", layout="wide")
 
 with open('model.pkl', 'rb') as file:
